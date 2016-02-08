26ebe46bd1bdddcb4e07374a7cf474c3123a1911
cwoodrow <chris.woodrow@serli.com>
2016-02-08 11:28:42 +0100

    modèles de données

--- src/main/java/nosql/workshop/model/suggest/TownSuggest.java
+++ src/main/java/nosql/workshop/model/suggest/TownSuggest.java
@@ -7,5 +7,38 @@ import java.util.List;
  * Town suggestion
  */
 public class TownSuggest {
+    private String townName;
+    private Double[] location;
 
+    private TownSuggest() {
+    }
+
+    public TownSuggest(String value, List<Double> location) {
+        this.townName = value;
+        this.location = location.toArray(new Double[location.size()]);
+    }
+
+    @Override
+    public String toString() {
+        return "TownSuggest{" +
+                "townName='" + townName + '\'' +
+                ", location=" + Arrays.toString(location) +
+                '}';
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public void setTownName(String townName) {
+        this.townName = townName;
+    }
+
+    public Double[] getLocation() {
+        return location;
+    }
+
+    public void setLocation(Double[] location) {
+        this.location = location;
+    }
 }
